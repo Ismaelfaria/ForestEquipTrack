5625e664fa7bfc1ec19e8e613b5c9d9930c6414a
limaismael <limaismael8901@gmail.com>
2024-08-14 08:41:28 -0300

    Configuração do Contexto e Criação do Repositório Genérico
    - Configurado o DbContext no Entity Framework Core para incluir as entidades Equipment, EquipmentState, EquipmentModel, EquipmentModelStateHourlyEarnings, EquipmentStateHistory e EquipmentPositionHistory.
    - Definidas as relações e chaves primárias compostas no método OnModelCreating para garantir a integridade referencial e o mapeamento adequado das entidades.
    - Criada a classe genérica RepositoryBase<TEntity> que implementa a interface IRepository<TEntity>, fornecendo métodos padrão para operações CRUD.
    - Adicionada a interface IRepository<TEntity> para definir os métodos básicos de um repositório, incluindo GetById, FindAll, Create, Update, e Delete.Configurar estrutura do projeto com arquitetura em camadas
    
    - Adicionado projeto MyApp.Web para a camada de apresentação
    - Adicionado projeto MyApp.Application para serviços de lógica de negócios
    - Adicionado projeto MyApp.Data para acesso a dados e persistência

---
+++ BusOnTime.Data/Entities/EquipmentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusOnTime.Data.Entities
+{
+    public class EquipmentModel
+    {
+        public Guid ModelId { get; set; }
+        public Guid EquipmentId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Equipment> Equipment { get; set; }
+        public ICollection<EquipmentModelStateHourlyEarnings> EquipmentModelStateHourlyEarnings { get; set; }
+
+    }
+}
---
+++ BusOnTime.Data/Interfaces/Generic/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BusOnTime.Data.Interfaces.Generic
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity GetById(Guid id);
+        IEnumerable<TEntity> FindAll();
+        TEntity Create(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(Guid id);
+    }
+}
