3b60e94866d22002b3ac7f994106bcf831ad9fce
limaismael <limaismael8901@gmail.com>
2024-08-19 19:23:55 -0300

    Criadas classes de controllers e Testes (ainda sem implementação).
    - Adicionada a pasta "BusOnTime.Tests" dentro da solução "BusOnTime", com 3 projetos XUnit para testes das camadas (Data, Application e Web).

---
+++ BusOnTime/Controllers/EquipmentStateHistoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BusOnTime.Web.Controllers
+{
+    public class EquipmentStateHistoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
