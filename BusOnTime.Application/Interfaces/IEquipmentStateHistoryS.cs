74e9ca23a3327c0244dbe7809027d291442b585c
Ismael Lima <119061742+Ismaelfaria@users.noreply.github.com>
2024-09-08 19:47:48 -0300

    Delete BusOnTime.Application/Interfaces directory

--- BusOnTime.Application/Interfaces/IEquipmentStateHistoryS.cs
+++
@@ -1,9 +0,0 @@
-﻿using ForestEquipTrack.Application.Interfaces.Generic;
-using ForestEquipTrack.Application.Mapping.DTOs.InputModel;
-using ForestEquipTrack.Application.Mapping.DTOs.ViewModel;
-
-namespace ForestEquipTrack.Application.Interfaces
-{
-    public interface IEquipmentStateHistoryS : IServiceBase<EquipmentStateHistoryIM, EquipmentStateHistoryVM>
-    {}
-}
